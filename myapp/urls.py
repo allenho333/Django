8b9fc3d6e2db85f00250f7a8cb165b7be0fb87b2
fcdn007 <fcdn007@gmail.com>
2024-10-13 08:59:53 +0800

    Update myapp.urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .views import ItemListCreate,ScrapeIns,ScrapeTiktok
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     # path('items/', ItemListCreate.as_view(), name='item-list-create'),
 	path('scrapeIns/', ScrapeIns.as_view(), name='scrapeIns'),
 	path('scrapeTiktok/', ScrapeTiktok.as_view(), name='scrapeTiktok'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
