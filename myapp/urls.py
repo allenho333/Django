c9bd22111ec452533751c982ef5e619b312110c5
Allen Ho <allenho@Allens-MacBook-Air.local>
2024-10-14 13:20:38 +1300

    nlp

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import ItemListCreate,ScrapeIns,ScrapeTiktok
 
 
-urlpatterns = [
+urlpatterns = [ 
     # path('items/', ItemListCreate.as_view(), name='item-list-create'),
 	path('scrapeIns/', ScrapeIns.as_view(), name='scrapeIns'),
 	path('scrapeTiktok/', ScrapeTiktok.as_view(), name='scrapeTiktok'),
